8756c35501345265de6639c08401ebe3923dedba
Tom Hinton <tom.hinton@cse.org.uk>
2024-01-04 20:04:08 +0000

    async waffle

--- custom_components/solar_lighting/switch.py
+++ custom_components/solar_lighting/switch.py
@@ -467,7 +467,7 @@ class MainSwitch(SwitchEntity, RestoreEntity):
 
                 if turn_on_twice:
                     _LOGGER.info("double turn-on: %s", turn_on_twice)
-                    asyncio.wait(
+                    await asyncio.wait(
                         [self.hass.async_create_task(
                             self.hass.services.async_call( call.domain, call.service, {ATTR_ENTITY_ID: e}, context=self.context ) )
                          for e in turn_on_twice]
